ce63d44a1f0f3ded259aae1f44f77b01b9d847b4
arbwndi <akunarimbi6272@gmail.com>
2026-06-05 11:31:25 +0700

    commit tambahan

--- app/Models/customer.php
+++ app/Models/customer.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class customer extends Model
+class Customer extends Model
 {
-    //
+    protected $fillable = [
+        'kode_customer',
+        'nama_customer',
+        'alamat',
+        'email',
+        'no_telp',
+    ];
 }
